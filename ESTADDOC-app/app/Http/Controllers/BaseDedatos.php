32e639b33cb9d4cd52b4ad79a2f315328aaf8810
SOPORTE TISOPORTE01SI 01 <SOPORTE01@CLIVERSALLES.COM.CO>
2023-12-06 17:07:03 -0500

    commit de prueba

---
+++ ESTADDOC-app/app/Http/Controllers/BaseDedatos.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BaseDedatos extends Controller
+{
+   /* public funtion index(){
+        $documentos = DB::table("sebas")->get();
+    }*/
+}
