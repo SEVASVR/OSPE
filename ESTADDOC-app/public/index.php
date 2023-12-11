a1a711f7b94aded12b49b52681ef7a5a481e3733
Ospi2 <jhuanlu321@gmail.com>
2023-12-11 07:39:57 -0500

    front1

--- ESTADDOC-app/public/index.php
+++ ESTADDOC-app/public/index.php
@@ -1,55 +1,41 @@
-<?php
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/jpg" href="img/favicon.png">
+    <title>Clinica Manizales</title>
+</head>
+<body>
+    <form action="" method="" id="Campos">
+    <div id="img">
+        <img class="img-header" src="img/ospedale.png" alt="ospedale.png">
+    </div>
+    <div class="mb-3">
+        <label for="check-label">Tipo de Documento</label>
+        <br>
+        <input type="text">
+        
+    </div>
+    <div class="mb-3">
+        <label for="check-labe l">Numero de Documento</label>
+        <br>
+        <input type="text">
+    </div>
+    <div class="button">
+        <button type="submit">Enviar</button>
+    </div>
+    </form>
+    <div class="mb-3" id="Info">
+        <label for="check-label">Tipo de <br> Documento</label>
+        <br>
+        <label for="check-label">Estado</label>
+        <br>
+        <label for="check-label">Numero de <br> Documento</label>
+    </div>
+    <div class="mb-3" id="Estado">
+        
+    </div>
+</body>
+</html>
